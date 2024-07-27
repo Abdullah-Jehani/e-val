3e8b80d8fc4f4da899a1c0eaa1404d0907fdc07e
sofiangk <sofian.a.r.gargouri@gmail.com>
2024-07-27 22:18:26 +0200

    refactor: Update FormHeader component layout
    Update the layout of the FormHeader component to improve the user experience. This includes adjusting the widths of the course and instructor sections to provide better alignment and spacing. These changes address recent user commits related to authentication and provide a more cohesive and visually appealing form header.

--- src/components/Student/Form/FormHeader.vue
+++ src/components/Student/Form/FormHeader.vue
@@ -3,7 +3,7 @@
     class="w-full flex lg:flex-row flex-col justify-between lg:items-center items-start my-4 lg:mt-8 gap-4 border-b pb-4 border-lightPurple"
   >
     <div
-      class="lg:w-2/3 w-full flex justify-start items-start lg:items-center gap-2"
+      class="lg:w-3/5 w-full flex justify-start items-start lg:items-center gap-2"
     >
       <!-- Course Name -->
       <label for="" class="text-xl font-normal text-darkGray">Course:</label>
@@ -14,7 +14,7 @@
 
     <!-- Instructor Name -->
     <div
-      class="lg:w-1/3 w-full flex justify-start items-start lg:items-center gap-2"
+      class="lg:w-2/5 w-full flex justify-start items-start lg:items-center gap-2"
     >
       <label for="" class="text-xl font-normal text-darkGray"
         >Instructor:</label
