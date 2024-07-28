c9410c87f6013fa7bf92c4abe2669f0cfe356f2c
sofiangk <sofian.a.r.gargouri@gmail.com>
2024-07-28 03:57:50 +0200

    Update RequestsTable.vue

--- src/components/Admin/RequestsTable.vue
+++ src/components/Admin/RequestsTable.vue
@@ -3,23 +3,6 @@
     <div class="flex flex-col justify-between items-center pb-0 bg-mainWhite">
       <div class="w-full flex justify-between items-center p-2 pb-4">
         <h1 class="text-2xl font-bold text-mainBlack">{{ cardTitle }}</h1>
-        <div class="text-darkPurple font-semibold">
-          <p class="hidden md:block">{{ tagName }}</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 md:hidden"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-            />
-          </svg>
-        </div>
       </div>
       <div id="divider" class="w-full h-px bg-lightPurple"></div>
     </div>
